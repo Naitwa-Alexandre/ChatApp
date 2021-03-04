70b27272cd97b2c6274f256ecbaaa8b432ea2dca
Alexandre Pereira <naitwa.alexandre@gmail.com>
2021-03-03 23:32:50 -0300

    Finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
-
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 
 const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine 
             height="100vh"
             projectID="6fdbe88e-9136-474e-8da0-9873b20ed3fe"
-            userName="Naitwa"
-            userSecret="eskalol0123"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     );
